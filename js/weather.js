a3c37a855e2f7750cd48f457f9c85948bc27e346
das1680 <das1680@naver.com>
2021-05-03 22:22:23 +0900

    폴더정리
    폴더 및 경로 정리

--- js/weather.js
+++ js/weather.js
@@ -12,32 +12,32 @@ function changebrightness(){
     var bri = brightness.innerHTML;
     x=parseInt(bri);
     if (x > 70) {
-        brightnessimg.src = 'high_brightness.png';
+        brightnessimg.src = '../web/img/high_brightness.png';
     } else if (x > 40) {
-        brightnessimg.src = 'middle_brightness.png';
-    } else { brightnessimg.src = 'low_brightness.png'; }
+        brightnessimg.src = '../web/img/middle_brightness.png';
+    } else { brightnessimg.src = '../web/img/low_brightness.png'; }
 }
 
 function changetemperature(){
     var tem = temperature.innerHTML;
     y=parseInt(tem);
     if (y > 20) {
-        temperatureimg.src = 'high_temperature.png';
+        temperatureimg.src = '../web/img/high_temperature.png';
     } else if (y > 10) {
-        temperatureimg.src = 'normal_temperature.png';
-    } else { temperatureimg.src = 'low_temperature.png'; }
+        temperatureimg.src = '../web/img/normal_temperature.png';
+    } else { temperatureimg.src = '../web/img/low_temperature.png'; }
 }
 
 function changetweather(){
     var wea = weather.innerHTML;
     if (wea == 'Sunny') {
-        weatherimg.src = 'sunny.png';
+        weatherimg.src = '../web/img/sunny.png';
     }
     if (wea == 'Windy') {
-        weatherimg.src = 'windy.png';
+        weatherimg.src = '../web/img/windy.png';
     }
     if (wea == 'Rainy') {
-        weatherimg.src = 'rainy.png';
+        weatherimg.src = '../web/img/rainy.png';
     }
 }
 
@@ -45,10 +45,10 @@ function changedust() {
     var dus = dust.innerHTML;
     y = parseInt(dus);
     if (y > 150) {
-        dustimg.src = 'bbad_dust.png';
+        dustimg.src = '../web/img/bbad_dust.png';
     } else if (y > 80) {
-        dustimg.src = 'bad_dust.png';
+        dustimg.src = '../web/img/bad_dust.png';
     } else if (y > 30) {
-        dustimg.src = 'normal_dust.png';
-    } else { dustimg.src = 'good_dust.png'; }
+        dustimg.src = '../web/img/normal_dust.png';
+    } else { dustimg.src = '../web/img/good_dust.png'; }
 }
