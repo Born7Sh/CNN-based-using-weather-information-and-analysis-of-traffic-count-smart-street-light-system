19f9aec605e0493409e9d2a34e3bba6408c9bf9a
das1680 <das1680@naver.com>
2021-05-07 16:02:16 +0900

    카메라 html 및 js에 영상 추가
    로그인페이지 파일 경로 수정

--- web/js/changecamera.js
+++ web/js/changecamera.js
@@ -2,19 +2,19 @@ function change2(){
     document.getElementById('camera1').className="nav-link active";
     document.getElementById('camera2').className="nav-link";
     document.getElementById('camera3').className="nav-link";
-    document.getElementById('camerachange').innerHTML=`<h1 id="camera_no" class="mt-4">Camera 1<img class="bulb" id="bulb" src="../web/img/bulb.png" onclick="br()"><span class="bulbbrightness">50%</span></h1><video class="cctv" autoplay muted controls width="1567px" id="videosource"><source src=""></video>`
+    document.getElementById('camerachange').innerHTML=`<h1 id="camera_no" class="mt-4">Camera 1<img class="bulb" id="bulb" src="../web/img/bulb.png" onclick="br()"><span class="bulbbrightness">50%</span></h1><img class="cctv float-left" src="http://222.237.154.71:8091/?action=stream">`
 }
 
 function change3(){
     document.getElementById('camera2').className="nav-link active";
     document.getElementById('camera1').className="nav-link";
     document.getElementById('camera3').className="nav-link";
-    document.getElementById('camerachange').innerHTML=`<h1 id="camera_no" class="mt-4">Camera 2<img class="bulb" id="bulb" src="../web/img/bulb.png" onclick="br()"><span class="bulbbrightness">50%</span></h1><video class="cctv" autoplay muted controls width="1567px" id="videosource"><source src=""></video>`
+    document.getElementById('camerachange').innerHTML=`<h1 id="camera_no" class="mt-4">Camera 2<img class="bulb" id="bulb" src="../web/img/bulb.png" onclick="br()"><span class="bulbbrightness">50%</span></h1><img class="cctv float-left" src="http://222.237.154.71:8091/?action=stream">`
 }
 
 function change1(){
     document.getElementById('camera3').className="nav-link active";
     document.getElementById('camera1').className="nav-link";
     document.getElementById('camera2').className="nav-link";
-    document.getElementById('camerachange').innerHTML=`<h1 id="camera_no" class="mt-4">Camera All<img class="bulb" id="bulb" src="../web/img/bulb.png" onclick="br()"><span class="bulbbrightness">50%</span></h1><video class="cctv2 float-left" autoplay muted controls width="780px" id="videosource"><source src=""></video><video class="cctv2 float-left" autoplay muted controls width="780px" id="videosource"><source src=""></video><div></div><video class="cctv2 float-left" autoplay muted controls width="780px" id="videosource"><source src=""></video><video class="cctv2 float-left" autoplay muted controls width="780px" id="videosource"><source src=""></video>`
+    document.getElementById('camerachange').innerHTML=`<h1 id="camera_no" class="mt-4">Camera All<img class="bulb" id="bulb" src="../web/img/bulb.png" onclick="br()"><span class="bulbbrightness">50%</span></h1><img class="cctv2 float-left" src="http://222.237.154.71:8091/?action=stream"><img class="cctv2 float-left" src="http://222.237.154.71:8091/?action=stream"><div></div><img class="cctv2 float-left" src="http://222.237.154.71:8091/?action=stream"><img class="cctv2 float-left" src="http://222.237.154.71:8091/?action=stream">`
 }
